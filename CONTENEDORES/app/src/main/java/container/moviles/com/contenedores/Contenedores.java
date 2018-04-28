54df0760c43c4560836c6d154d6ad3fc851227d9
cmadrigalq <cmadrigal.3192@gmail.com>
2018-04-27 23:00:22 -0600

    terminado

--- CONTENEDORES/app/src/main/java/container/moviles/com/contenedores/Contenedores.java
+++ CONTENEDORES/app/src/main/java/container/moviles/com/contenedores/Contenedores.java
@@ -5,18 +5,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TabHost;
+import android.widget.Toast;
 
 public class Contenedores extends AppCompatActivity {
-    //RadioButton r1 = (RadioButton) findViewById(R.id.Radio1);
-    /*RadioButton r2 = (RadioButton) findViewById(R.id.Radio2);
-    RadioButton r3 = (RadioButton) findViewById(R.id.Radio3);
-    RadioButton r4 = (RadioButton) findViewById(R.id.radio4);
-    RadioButton r5 = (RadioButton) findViewById(R.id.radio5);
-    RadioButton r6 = (RadioButton) findViewById(R.id.radio6);*/
-    /*RadioGroup radiogroup = (RadioGroup) findViewById(R.id.rg);
-    Button btn = (Button) findViewById(R.id.btn);
+
+    RadioButton r1,r2,r3,r4,r5,r6;
+    RadioGroup radiogroup ;
+    Button btn;
+    TabHost th;
+    void initCosas(){
+        r1 = (RadioButton) findViewById(R.id.Radio1);
+        r2 = (RadioButton) findViewById(R.id.Radio2);
+        r3 = (RadioButton) findViewById(R.id.Radio3);
+        r4 = (RadioButton) findViewById(R.id.radio4);
+        r5 = (RadioButton) findViewById(R.id.radio5);
+        r6 = (RadioButton) findViewById(R.id.radio6);
+        radiogroup = (RadioGroup) findViewById(R.id.rg);
+        btn = (Button) findViewById(R.id.btn);
+    }
 
     String[] horarios_yoga = {
             "Lunes 10:00a.m.", "Lunes 6:00p.m.", "Martes 8:00a.m.",
@@ -42,24 +55,25 @@ public class Contenedores extends AppCompatActivity {
             "Lunes 8:00a.m.", "Lunes 6:00p.m.", "Martes 8:00a.m.",
             "Miércoles 6:00p.m.", "Jueves 9:00a.m.", "Domingo 8:00a.m."
     };
-    */
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contenedore);
+        initCosas();
         init();
-        //BotonClases();
     }
 
     void init() {
-        TabHost th = (TabHost) super.findViewById(R.id.host);
+        th =  (TabHost)super.findViewById(R.id.host);
         th.setup();
         TabHost.TabSpec ts1 = th.newTabSpec("t1");
         ts1.setIndicator("CLASES");
         ts1.setContent(R.id.Radio_Group);
         th.addTab(ts1);
+        BotonClases();
 
         TabHost.TabSpec ts2 = th.newTabSpec("t2");
         ts2.setIndicator("HORARIOS");
@@ -70,15 +84,6 @@ public class Contenedores extends AppCompatActivity {
         ts3.setIndicator("GALERIA");
         ts3.setContent(R.id.Grid);
         th.addTab(ts3);
-
-        TabHost.TabSpec ts4 = th.newTabSpec("t4");
-        ts4.setIndicator("RUTINAS");
-        ts4.setContent(R.id.WebV);
-        th.addTab(ts4);
-        WebView myWebView = (WebView) findViewById(R.id.wb);
-        myWebView.loadUrl("https://www.youtube.com/user/gymvirtual/playlists");
-
-
         GridView gridview = (GridView) findViewById(R.id.gridview1);
         gridview.setAdapter(new ImageAdapter(this));
         gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -87,76 +92,62 @@ public class Contenedores extends AppCompatActivity {
 
             }
         });
+
+        TabHost.TabSpec ts4 = th.newTabSpec("t4");
+        ts4.setIndicator("RUTINAS");
+        ts4.setContent(R.id.WebV);
+        th.addTab(ts4);
+        WebView myWebView = (WebView) findViewById(R.id.wb);
+
+        //myWebView.loadUrl("https://www.youtube.com/user/gymvirtual/playlists");
+        //myWebView.loadUrl("https://www.google.com");
+        th.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                if( tabId.equals("t4")){
+                    WebView myWebView = (WebView) findViewById(R.id.wb);
+
+                    myWebView.loadUrl("https://www.youtube.com/user/gymvirtual/playlists");
+
+                }
+            }
+        });
     }
 
-/*
-
-    public void onRadioButtonChecked(View view) {
-        boolean checked = ((RadioButton) view ).isChecked();
-        switch(view.getId()) {
-            case R.id.Radio1:
-                if (checked)
-                    listaHorarios(horarios_yoga);
-                    break;
-            case R.id.Radio2:
-                if (checked)
-                    listaHorarios(horarios_pilates);
-                    break;
-            case R.id.Radio3:
-                if (checked)
-                    listaHorarios(horarios_funcional);
-                    break;
-            case R.id.radio4:
-                if (checked)
-                    listaHorarios(horarios_crossfit);
-                    break;
-            case R.id.radio5:
-                if (checked)
-                    listaHorarios(horarios_danza);
-                    break;
-            case R.id.radio6:
-                if (checked)
-                    listaHorarios(horarios_zumba);
-                    break;
-        }
-    }
 public void BotonClases(){
     btn.setOnClickListener(new View.OnClickListener() {
 
         @Override
-        public void onClick (View v){
-            // get selected radio button from radioGroup
-            int selectedId = radiogroup.getCheckedRadioButtonId();
-            // find the radio button by returned id
-            RadioButton radioButton = (RadioButton) findViewById(selectedId);
-            Toast.makeText(Contenedores.this,
-                    radioButton.getText(), Toast.LENGTH_SHORT).show();
-            switch (radioButton) {
-                case (radioButton = (RadioButton) r1):
-
-                    Toast.makeText(Contenedores.this,
-                            radioButton.getText(), Toast.LENGTH_SHORT).show();
+        public void onClick (View view){
+            int selectedId = radiogroup.getCheckedRadioButtonId();// get selected radio button from radioGroup
+            RadioButton radioButton = (RadioButton) findViewById(selectedId);// find the radio button by returned id
+            switch (radioButton.getId()) {
+                case (R.id.Radio1):
+                    listaHorarios(horarios_yoga);
                     break;
                 case R.id.Radio2:
-                    //listaHorarios(horarios_pilates);
+                    listaHorarios(horarios_pilates);
                     break;
                 case R.id.Radio3:
-                    //listaHorarios(horarios_funcional);
+                    listaHorarios(horarios_funcional);
                     break;
                 case R.id.radio4:
-                    //listaHorarios(horarios_crossfit);
+                    listaHorarios(horarios_crossfit);
                     break;
                 case R.id.radio5:
-                    //listaHorarios(horarios_danza);
+                    listaHorarios(horarios_danza);
                     break;
                 case R.id.radio6:
-                   //listaHorarios(horarios_zumba);
+                   listaHorarios(horarios_zumba);
                     break;
             }
+            Contenedores.this.th.setCurrentTab(1);
+            Toast.makeText(Contenedores.this,
+                    radioButton.getText(), Toast.LENGTH_SHORT).show();
         }
     });
- }*/
-    /*void listaHorarios(String[] list) {
+ }
+    void listaHorarios(String[] list) {
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(
                         this,
@@ -165,8 +156,5 @@ public void BotonClases(){
                 );
         ListView lista = (ListView)super.findViewById(R.id.Lista);
         lista.setAdapter(adapter);
-    }*/
+    }
 }
-
-/* Toast.makeText(MainActivity.this,
-                    radioButton.getText(), Toast.LENGTH_SHORT).show();*/
